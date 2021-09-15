e3bb3421b0e495499eaa495769385238450f393a
chapterjason <jason@sourecode.dev>
2021-09-15 08:58:16 +0200

    feat: Add test component

---
+++ DatabaseTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SoureCode\Component\Test;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @author Jason Schilling <jason@sourecode.dev>
+ */
+trait DatabaseTrait
+{
+    /**
+     * @template T
+     *
+     * @param class-string<T> $className
+     *
+     * @return EntityRepository<T>
+     */
+    protected function getRepository(string $className, string $managerName = null): EntityRepository
+    {
+        $manager = $this->getEntityManager($managerName);
+
+        return $manager->getRepository($className);
+    }
+
+    protected function getEntityManager($managerName = null): EntityManagerInterface
+    {
+        $doctrine = $this->getDoctrine();
+        $managerName = $managerName ?? $doctrine->getDefaultManagerName();
+
+        return $doctrine->getManager($managerName);
+    }
+
+    protected function getDoctrine(): Registry
+    {
+        $container = static::getContainer();
+
+        return $container->get('doctrine');
+    }
+}
---
+++ UrlTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SoureCode\Component\Test;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * @author Jason Schilling <jason@sourecode.dev>
+ */
+trait UrlTrait
+{
+    public function generateUrl(
+        string $name,
+        array $parameters = [],
+        int $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH
+    ): string {
+        $container = self::getContainer();
+
+        /**
+         * @var UrlGeneratorInterface $urlGenerator
+         */
+        $urlGenerator = $container->get(UrlGeneratorInterface::class);
+
+        return $urlGenerator->generate($name, $parameters, $referenceType);
+    }
+}
